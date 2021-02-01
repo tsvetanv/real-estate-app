3cd63e7a2807caa5e441454ce55093eff60c7441
Tsvetan Vasilev <tsvetan.vasilev@gmail.com>
2021-02-02 00:13:07 +0200

    Setup Maven project skeleton.

---
+++ src/main/java/com/tsvetanv/resa/ResaApplication.java
@@ -0,0 +1,13 @@
+package com.tsvetanv.resa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tsvetanv/resa/ResaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsvetanv.resa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
